abba4c8ac64e968ea7570ebfaebe5c5a94933142
coderlab-tech-mg <coderlab.tech@proton.me>
2024-07-23 11:54:29 +0300

    chore: configure flyway test location

---
+++ src/main/resources/db/testdata/V99_1__Test_create_customers.sql
@@ -0,0 +1,2 @@
+insert into customer
+values ('customer-id-1', 'Paul Updated', 'paul.updated@gmail.com');
---
+++ src/test/java/pro/craftlab/voucher/conf/FlywayMigrationConf.java
@@ -0,0 +1,10 @@
+package pro.craftlab.voucher.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+public class FlywayMigrationConf {
+
+    void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.flyway.locations", () -> "classpath:/db/migration,classpath:/db/testdata");
+    }
+}
