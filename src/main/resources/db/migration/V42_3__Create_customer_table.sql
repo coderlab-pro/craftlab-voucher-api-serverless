954d1921010a597d1dc74f3b6beb0b8791471ef6
coderlab-tech-mg <coderlab.tech@proton.me>
2024-07-23 11:55:07 +0300

    chore: re-write customers sql script

--- src/main/resources/db/migration/V42_3__Create_customer_table.sql
+++ src/main/resources/db/migration/V42_3__Create_customer_table.sql
@@ -3,6 +3,3 @@ create table if not exists customer
     id varchar
         constraint customer_pk primary key
 );
-
-insert into customer (id)
-values ('customer-id-1')
--- src/main/resources/db/migration/V42_4__add_table_customer.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE customer ADD COLUMN name VARCHAR;
-ALTER TABLE customer ADD COLUMN mail VARCHAR;
-
-
-UPDATE customer
-SET name = 'Paul Updated', mail = 'paul.updated@gmail.com'
-WHERE id = 'customer-id-1';
-
