09b6534a175971af88ef4f93a7ed0a0a63fa3f73
coderlab-tech-mg <coderlab.tech@proton.me>
2024-07-23 11:54:48 +0300

    chore: add @Builder to Customer

--- src/main/java/pro/craftlab/voucher/repository/model/Customer.java
+++ src/main/java/pro/craftlab/voucher/repository/model/Customer.java
@@ -3,6 +3,7 @@ package pro.craftlab.voucher.repository.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Customer {
   @Id private String id;
   private String name;
