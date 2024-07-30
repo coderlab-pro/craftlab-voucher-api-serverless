824b5365cfbf4244093ebfcbd31718b41a6f584a
coderlab-rija <coderlab.rija.r@proton.me>
2024-07-30 16:04:31 +0300

    chore: change LAZY to EAGER

--- src/main/java/pro/craftlab/voucher/repository/model/Customer.java
+++ src/main/java/pro/craftlab/voucher/repository/model/Customer.java
@@ -1,18 +1,17 @@
 package pro.craftlab.voucher.repository.model;
 
+import static jakarta.persistence.FetchType.EAGER;
+
 import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static jakarta.persistence.FetchType.LAZY;
 
 @Data
 @Entity
@@ -24,7 +23,6 @@ public class Customer {
   private String name;
   private String mail;
 
-  @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true, fetch = LAZY)
+  @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true, fetch = EAGER)
   private Set<Voucher> vouchers = new HashSet<>();
-
 }
