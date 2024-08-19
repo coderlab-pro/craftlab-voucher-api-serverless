8f1f5684f621155e2ac8bce442fd0e534e0a541f
coderlab-tech-mg <coderlab.tech@proton.me>
2024-08-19 16:02:17 +0300

    chore: reformat test create voucher script

--- src/main/resources/db/testdata/V99_2__Test_create_voucher.sql
+++ src/main/resources/db/testdata/V99_2__Test_create_voucher.sql
@@ -1,2 +1,2 @@
-insert into voucher (id,code, validation_datetime, creation_datetime, customer_id)
-values ('voucher-id-i','voucher1234', '2024-07-31 12:00:00+00', '2024-08-30 12:00:00+00',  'customer-id-1');
+insert into voucher (id, code, validation_datetime, creation_datetime, customer_id)
+values ('voucher-id-i', 'voucher1234', '2024-07-31 12:00:00+00', '2024-08-30 12:00:00+00', 'customer-id-1');
