d96076b6a695843cb41bba5152d99f54ca599fbb
coderlab-rija <coderlab.rija.r@proton.me>
2024-08-22 14:05:22 +0300

    chore: test to increase overage

--- src/main/java/pro/craftlab/voucher/repository/function/VoucherValidation.java
+++ src/main/java/pro/craftlab/voucher/repository/function/VoucherValidation.java
@@ -2,13 +2,14 @@ package pro.craftlab.voucher.repository.function;
 
 import java.time.Instant;
 import org.springframework.stereotype.Component;
+import pro.craftlab.voucher.repository.model.exception.BadRequestException;
 
 @Component
 public class VoucherValidation {
 
   public boolean isValidDate(Instant date) {
     if (date == null) {
-      throw new IllegalArgumentException("Date cannot be null");
+      throw new BadRequestException("Date Validation invalid");
     }
     return date.isAfter(Instant.now());
   }
